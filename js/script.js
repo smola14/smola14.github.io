d31cb766ce99bd363c3cfac4fd9188e4d1de9f87
smola14 <dominik.smolinsky@icloud.com>
2022-03-10 18:29:47 +0100

    footer submit button

--- js/script.js
+++ js/script.js
@@ -68,24 +68,3 @@ function navHighlighter() {
     }
   })
 }
-
-// const sections = document.querySelectorAll('section')
-// const navLi = document.querySelectorAll('nav li')
-// window.addEventListener('scroll', () => {
-//   let current = ''
-//   sections.forEach((section) => {
-//     const sectionTop = section.offsetTop
-//     const sectionHeight = section.clientHeight
-//     if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
-//       current = section.getAttribute('id')
-//     }
-//   })
-
-//   navLi.forEach((li) => {
-//     li.classList.remove('active')
-//     if (li.classList.contains(current)) {
-//       li.classList.add('active')
-//       console.log(li)
-//     }
-//   })
-// })
